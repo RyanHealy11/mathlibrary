9080bc0ea226c48885d8a5e29e47659fe6797bdf
Ryan Healy <ryan.healy11@gmail.com>
2019-01-11 10:39:09 -0800

    parents

--- mathstuff/transform2d.cpp
+++ mathstuff/transform2d.cpp
@@ -17,8 +17,7 @@ void transform2d::rotate(const float angle)
 
 void transform2d::lookAt(const transform2d & target)
 {
-	translate(target.localPos);
-	rotate(target.localRot);
+	
 }
 
 vec2 transform2d::forward() const
@@ -32,5 +31,71 @@ void transform2d::setForward(const vec2 & newFwd)
 
 mat3 transform2d::getTRSMatrix() const
 {
-	return mat3();
+	return mat3::translation(localPos) * mat3::rotation(localRot) * mat3::scale(localScale);
+}
+
+vec2 transform2d::worldPosition() const
+{
+	vec2 result;
+	result += localPos;
+	if (parent == NULL) { return result; }
+	result += parent->worldPosition();
+	return result;
+}
+
+float transform2d::worldRotation() const
+{
+	float result = 0.0f;
+	result += localRot;
+	if (parent == NULL) { return result; }
+	result += parent->worldRotation();
+	return result;
+}
+
+vec2 transform2d::worldScale() const
+{
+	vec2 result;
+	result += localScale;
+	if (parent == NULL) { return result; }
+	result += parent->worldScale();
+	return result;
+}
+
+void transform2d::setParent(transform2d * newparent)
+{
+	parent = newparent;
+	parent->children.push_back(this);
+}
+
+transform2d * transform2d::getParent()
+{
+	return parent;
+}
+
+transform2d const * transform2d::getParent() const
+{
+	return parent;
+}
+
+transform2d * transform2d::getChildren()
+{
+	if (!children[0] == NULL) {
+		return children[0];
+	}
+	std::cout << "yall are baren" << std::endl;
+	return NULL;
+}
+
+transform2d * const * transform2d::getChildren() const
+{
+	if (!children[0] == NULL) {
+		return &children[0];
+	}
+	std::cout << "yall are baren" << std::endl;
+	return NULL;
+}
+
+size_t transform2d::getChildrenCount() const
+{
+	return children.size();
 }
